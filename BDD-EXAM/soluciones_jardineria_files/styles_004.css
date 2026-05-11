8a95f62d8d087040b790a46dfc776110d1642c6c
Tak_0 on Desktop(Arch) <tmtroshko@gmail.com>
2026-05-12 00:02:38 +0200

    Staged

---
+++ BDD-EXAM/soluciones_jardineria_files/styles_004.css
@@ -0,0 +1,4 @@
+.block_completion_progress .progressBarCell.completed { background-color: #73A839; }
+.block_completion_progress .progressBarCell.submittedNotComplete { background-color: #FFCC00; }
+.block_completion_progress .progressBarCell.notCompleted { background-color: #C71C22; }
+.block_completion_progress .progressBarCell.futureNotCompleted { background-color: #025187; }
